11062a1a52ae990de97fd94145d174f00b37aed3
bofh <bofh@gameorama.de>
2016-06-17 15:26:59 +0200

    test

--- lib/Singleton.php
+++ lib/Singleton.php
@@ -1,15 +1,32 @@
 <?php
+
+/**
+ * Class Singelton stellt sicher, dass es nur eine Instance gibt
+ *
+ * Warnung:
+ * Die Verwendung des Singelton ist ein Antipattern, wenn es nur eine Instance gibt, warum dann keine Klasse mit nur
+ * statischen Funktionen und statischen Variablen verwenden?
+ */
 class Singleton extends Base
 {
-	protected  static $instance;
+	
 
-	static function getInstance()
+	protected function __construct()
 	{
-		if ( static::$instance === null )
-		{
+		//Thou shalt not construct that which is unconstructable!
+	}
 
-			static::$instance = new static();
+	protected function __clone()
+	{
+		
+	}
+	
+	public static function getInstance()
+	{
+		static $instance=null;
+		if ($instance === null) {
+			$instance = new static();
 		}
-		return static::$instance;
+		return $instance;
 	}
 }
